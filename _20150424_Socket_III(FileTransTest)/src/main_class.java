65cffe6f3bac36106151e11bb6f4250ed27a3eb0
san9014 <san9014@gmail.com>
2015-04-27 14:39:00 +0900

    [2015.04.27] commit and push (Socket_III)

--- _20150424_Socket_III(FileTransTest)/src/main_class.java
+++ _20150424_Socket_III(FileTransTest)/src/main_class.java
@@ -2,8 +2,10 @@ public class main_class {
 
 	public static void main(String[] args) {
 		//Branch_I으로 바뀜
-		_Server serverT = new _Server();
-		serverT.serverStart();
+		//_Server serverT = new _Server();
+		//serverT.serverStart();
+		
+		_ServerTest test = new _ServerTest();
+		test.mainServer();
 	}
-
 }
